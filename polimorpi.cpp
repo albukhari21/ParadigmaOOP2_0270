ffb5aba5c1093361d0e734ac888e76d7cd2c3ac9
albukhari21 <albukhariinsankamil@gmail.com>
2025-06-03 10:04:26 +0700

    Create 'seseorang' class with default virtual method 'pesan'

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+class seseorang {
+public:    
+    //virtual void pesan
+    virtual void pesan() {
+        cout << "Pesan dari seseorang" << endl;
+    }
+};
