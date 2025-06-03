6483753a6a25eb4977012d031113ddd47bd3e5de
albukhari21 <albukhariinsankamil@gmail.com>
2025-06-03 10:54:02 +0700

    Isi nama saklar lampu dengan metode setSaklarNo di main

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -16,3 +16,10 @@ public:
 int main() {
     remotelampu lampuRumah;
 
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+
+}
